5899940c026a9e6ebce7bb3e5459c20e2570bcf1
hernic <nicolas.hernandez@aleph-networks.com>
2024-02-23 11:26:15 +0100

    remove bugs finish Homoglyphs

--- src/com/aleph/graymatter/jtyposquatting/generator/Homoglyph.java
+++ src/com/aleph/graymatter/jtyposquatting/generator/Homoglyph.java
@@ -13,7 +13,7 @@ public class Homoglyph {
         String domainWithoutTLD = DomainName.getDomainWithoutTLD(domainName.toString());
         String TLD = domainName.getTLD();
 
-        if (domainName.toString().indexOf('a') != -1) {
+        if (domainWithoutTLD.indexOf('a') != -1) {
             stringBuilder = new StringBuilder(domainWithoutTLD);
 
             idx = stringBuilder.indexOf("a");
@@ -22,7 +22,7 @@ public class Homoglyph {
                 resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
             }
         }
-        if (domainName.toString().indexOf('b') != -1) {
+        if (domainWithoutTLD.indexOf('b') != -1) {
             stringBuilder = new StringBuilder(domainWithoutTLD);
             idx = stringBuilder.indexOf("b");
             for (int i = 0; i < Const.SIMILAR_CHAR.get('b').length; i++) {
@@ -30,7 +30,7 @@ public class Homoglyph {
                 resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
             }
         }
-        if (domainName.toString().indexOf('c') != -1) {
+        if (domainWithoutTLD.indexOf('c') != -1) {
             stringBuilder = new StringBuilder(domainWithoutTLD);
             idx = stringBuilder.indexOf("c");
             for (int i = 0; i < Const.SIMILAR_CHAR.get('c').length; i++) {
@@ -38,7 +38,7 @@ public class Homoglyph {
                 resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
             }
         }
-        if (domainName.toString().indexOf('d') != -1) {
+        if (domainWithoutTLD.indexOf('d') != -1) {
             stringBuilder = new StringBuilder(domainWithoutTLD);
             idx = stringBuilder.indexOf("d");
             for (int i = 0; i < Const.SIMILAR_CHAR.get('d').length; i++) {
@@ -46,7 +46,7 @@ public class Homoglyph {
                 resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
             }
         }
-        if (domainName.toString().indexOf('e') != -1) {
+        if (domainWithoutTLD.indexOf('e') != -1) {
             stringBuilder = new StringBuilder(domainWithoutTLD);
             idx = stringBuilder.indexOf("e");
             for (int i = 0; i < Const.SIMILAR_CHAR.get('e').length; i++) {
@@ -54,7 +54,174 @@ public class Homoglyph {
                 resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
             }
         }
-
+        if (domainWithoutTLD.indexOf('f') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("f");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('f').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('f')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('g') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("g");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('g').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('g')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('h') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("h");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('h').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('h')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('i') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("i");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('i').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('i')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('j') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("j");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('j').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('j')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('k') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("k");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('k').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('k')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('l') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("l");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('l').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('l')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('m') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("m");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('m').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('m')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('n') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("n");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('n').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('n')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('o') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("o");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('o').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('o')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('p') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("p");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('p').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('p')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('q') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("q");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('q').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('q')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('r') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("r");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('r').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('r')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('s') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("s");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('s').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('s')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('t') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("t");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('t').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('t')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('u') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("u");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('u').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('u')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('v') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("v");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('v').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('v')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('w') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("w");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('w').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('w')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('x') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("x");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('x').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('x')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('y') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("y");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('z').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('y')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
+        if (domainWithoutTLD.indexOf('z') != -1) {
+            stringBuilder = new StringBuilder(domainWithoutTLD);
+            idx = stringBuilder.indexOf("z");
+            for (int i = 0; i < Const.SIMILAR_CHAR.get('z').length; i++) {
+                stringBuilder.replace(idx, idx + 1, Const.SIMILAR_CHAR.get('z')[i]);
+                resultList.add(new DomainName(stringBuilder.toString()+'.'+TLD));
+            }
+        }
     }
 }
 
