bff4baabd6880945812fccfcbf79ff79ab19294f
hernic <nicolas.hernandez@aleph-networks.com>
2024-03-24 18:26:52 +0100

    add icon

--- src/com/aleph/graymatter/jtyposquatting/ui/JTypoFrame.java
+++ src/com/aleph/graymatter/jtyposquatting/ui/JTypoFrame.java
@@ -9,7 +9,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static javax.imageio.ImageIO.read;
 
 public class JTypoFrame extends JFrame implements ActionListener, KeyListener {
 
@@ -18,7 +22,8 @@ public class JTypoFrame extends JFrame implements ActionListener, KeyListener {
     private final JTextArea jTextAreaConsole;
     private final JLabel numberLabel;
 
-    public JTypoFrame() {
+    public JTypoFrame() throws IOException {
+        setIconImage(read(new File("res/aleph_sg.jpg")));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setTitle("Aleph TypoSquatting Tool");
